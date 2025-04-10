5ee537dc775c0f9e185fd9a3d4fc8ceacf274710
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-04-10 11:03:52 +0900

    [Silver I] Title: 점프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+board = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0]*N for _ in range(N)]
+dp[0][0] = 1
+
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == 0 or dp[i][j] == 0:
+            continue
+        
+        jump = board[i][j]
+        
+        if i + jump < N:
+            dp[i + jump][j] += dp[i][j]
+        if j + jump < N:
+            dp[i][j + jump] += dp[i][j]
+print(dp[N-1][N-1])

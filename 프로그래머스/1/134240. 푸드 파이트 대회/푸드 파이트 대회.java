7ae4117ec9f2bdfd94bac992b7fe1ef4a13ebdda
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-10-03 22:52:59 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.54 ms, Memory: 92.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                sb.append(i);
+            }
+        }
+
+        String left = sb.toString();
+        String right = new StringBuilder(left).reverse().toString();
+
+        return left + "0" + right;
+    }
+}

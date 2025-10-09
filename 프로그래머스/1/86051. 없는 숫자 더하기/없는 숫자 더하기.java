bd09860af1a7b72f09679e4f3fd129a65d07fec7
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-10-09 16:01:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
+        }
+        return sum;
+    }
+}

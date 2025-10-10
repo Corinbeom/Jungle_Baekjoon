50f1cff2ec41f79db667d7ab0bf7d73d511836b0
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-10-10 12:23:44 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 8.90 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length <= 1) {
+            return new int[]{-1};
+        }
+
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            if (num != min) {
+                list.add(num);
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}

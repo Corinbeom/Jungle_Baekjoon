a7c94ecfed51b99ae9974e2125ac976ecc9e794d
Corinbeom <105149196+Corinbeom@users.noreply.github.com>
2025-10-17 15:41:10 +0900

    [level 0] Title: n 번째 원소부터, Time: 3.27 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for (int i = n - 1; i < num_list.length; i++) {
+            arr.add(num_list[i]);
+        }
+        return arr.stream().mapToInt(i -> i).toArray();
+
+    }
+}
